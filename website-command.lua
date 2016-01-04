0d88769990ee723b3424968d6d7d1f6877dd5e98
Billard <foresome1249@gmail.com>
2016-01-04 13:48:24 -1000

    Update website-command.lua

--- website-command.lua
+++ website-command.lua
@@ -1,7 +1,6 @@
-local WebsiteLink = "www.google.com"
-hook.Add( "PlayerSay", "WebsiteCommandHook", function( sender, text, teamchat )
-if !sender:IsValid() then return end
-if text == "!website" then
-	gui.OpenURL( WebsiteLink )
-end
-)
+local WebsiteLink = "www.google.com" --Save the URL in an easy-to-configure variable
+hook.Add( "PlayerSay", "WebsiteCommandHook", function( sender, text, teamchat ) --Hook on to player chat
+if !sender:IsValid() then return end --Check if the sender of the chat exists, if not then bail
+if text == "!website" then --If they type this then
+	gui.OpenURL( WebsiteLink ) --Open this link in steam overlay
+end ) --End the hook to prevent errors
