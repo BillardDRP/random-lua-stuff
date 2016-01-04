e37c1dc47aaadb70d3dea34fab6cce94c3406e23
Billard <foresome1249@gmail.com>
2016-01-04 12:26:26 -1000

    Update website-command.lua

--- website-command.lua
+++ website-command.lua
@@ -1,1 +1,7 @@
-
+local WebsiteLink = "www.google.com"
+hook.Add( "PlayerSay", "WebsiteCommandHook", function( sender, text, teamchat )
+if !sender:IsValid() then return end
+if text == "!website" then
+	gui.OpenURL( WebsiteLink )
+end
+)
