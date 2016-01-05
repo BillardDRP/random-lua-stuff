cadd2f69bdde43e8a973742d6744212ca9c82b9a
Billard <foresome1249@gmail.com>
2016-01-04 17:44:17 -1000

    Update sv_rollthedice.lua

--- sv_rollthedice.lua
+++ sv_rollthedice.lua
@@ -5,11 +5,35 @@ RTD.Rolls = { --Use this to add more rolls but only if you crated the function b
 	[RTD_SpeedDecrease] = true
 	[RTD_HealthIncrease] = true
 	[RTD_HealthDecrease] = true
-	[RTD_Drugged] = true
 	[RTD_JumpIncrease] = true
 	[RTD_JumpDecrease] = true
+	[RTD_Drugged] = true
 ]
 
 function RTD_SpeedIncrease( ply )
-	
+	local become_message = "became a cheetah!"
+end
+
+function RTD_SpeedDecrease( ply )
+	local become_message = "became a snail!"
+end
+
+function RTD_HealthIncrease( ply )
+	local become_message = "took some morphine!"
+end
+
+function RTD_HealthDecrease( ply )
+	local become_message = "caught ebola!"
+end
+
+function RTD_JumpIncrease( ply )
+	local become_message = "became a bunny!"
+end
+
+function RTD_JumpDecrease( ply )
+	local become_message = "became an anvil!"
+end
+
+function RTD_JumpDecrease( ply )
+	local become_message = "took some of that good stuff!"
 end
