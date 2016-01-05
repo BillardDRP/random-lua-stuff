598ae18b0eab878b051c7be8db08250c29dba873
Billard <foresome1249@gmail.com>
2016-01-05 12:48:27 -1000

    Update and rename sv_luckymoney.lua to sv_adminraffle.lua

---
+++ sv_adminraffle.lua
@@ -0,0 +1,11 @@
+AdminRaffle = {}
+AdminRaffle.DefaultChance = 5 --Chance out of one hundred, can be overridden using console
+AdminRaffle.DefaultAmount = 800 --Money earned upon win, can be overridden using console
+
+concommand.Add( "darkrp_adminraffle_start", function( ply, cmd, args )
+	for k, v in pairs( player.GetAll ) do
+		v:ChatPrint( "The server admins have started a raffle!" )
+	end
+	local RaffleChance = args[2] or AdminRaffle.DefaultChance
+	local RaffleMoney = args[1] or AdminRaffle.DefaultAmount
+end )
