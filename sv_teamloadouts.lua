9585117492a8096da88f3de9b5a5c1d331b08bb2
Billard <foresome1249@gmail.com>
2016-01-04 19:52:58 -1000

    Update sv_teamloadouts.lua

--- sv_teamloadouts.lua
+++ sv_teamloadouts.lua
@@ -1,1 +1,19 @@
+function SetContains(set, key)
+    return set[key] ~= nil
+end
 
+TeamLoadouts = {
+	["Gun Dealer"] = { "weapon_crowbar", "weapon_pistol", "lockpick" }
+	["Mayor"] = { "stunstick", "weapon_pistol" }
+	["Gangster"] = { "weapon_mac102", "weapon_deagle2" }
+}
+
+hook.Add( "PlayerSpawn", "TeamLoadoutsMainHook" function( player )
+	local PlayerTeamName = team.GetName( player:Team() )
+	if SetContains( TeamLoadouts, PlayerTeamName ) then
+		for k, v in pairs( TeamLoadouts[PlayerTeamName] ) do
+			player:Give( v )
+			print( "You have been given a "..v:GetPrintName() )
+		end
+	end
+end )
