383831431f5a981d76faa64efd332354d37a7a52
Billard <foresome1249@gmail.com>
2016-01-04 17:35:02 -1000

    Update sv_rollthedice.lua

--- sv_rollthedice.lua
+++ sv_rollthedice.lua
@@ -1,1 +1,15 @@
+RTD = {}
+RTD.CooldownTime = 20 --Cooldown time in seconds
+RTD.Rolls = { --Use this to add more rolls but only if you crated the function beforehand
+	[RTD_SpeedIncrease] = true
+	[RTD_SpeedDecrease] = true
+	[RTD_HealthIncrease] = true
+	[RTD_HealthDecrease] = true
+	[RTD_Drugged] = true
+	[RTD_JumpIncrease] = true
+	[RTD_JumpDecrease] = true
+]
 
+function RTD_SpeedIncrease( ply )
+	
+end
