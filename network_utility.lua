eab5ccd7f4b08591639d27ebcc377793a3d76d30
Billard <foresome1249@gmail.com>
2016-01-05 12:16:11 -1000

    Update network_utility.lua

--- network_utility.lua
+++ network_utility.lua
@@ -1,1 +1,8 @@
-
+NU = {}
+NU.SendStringToClient = function( theFile, theString )
+	if not theFile then return end
+	util.AddNetworkString( tostring( theFile ) )
+	net.Start( tostring( theFile ) )
+	net.WriteString( tostring( theString ) )
+	net.Send()
+end
