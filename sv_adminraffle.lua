69680f449b9ab79dba926154c36f8330bfdd35a8
Billard <foresome1249@gmail.com>
2016-01-05 12:54:27 -1000

    Update sv_adminraffle.lua

--- sv_adminraffle.lua
+++ sv_adminraffle.lua
@@ -3,6 +3,7 @@ AdminRaffle.DefaultChance = 5 --Chance out of one hundred, can be overridden usi
 AdminRaffle.DefaultAmount = 800 --Money earned upon win, can be overridden using console
 
 concommand.Add( "darkrp_adminraffle_start", function( ply, cmd, args )
+	if not ply:IsAdmin() then return end
 	for k, v in pairs( player.GetAll ) do
 		v:ChatPrint( "The server admins have started a raffle!" )
 	end
