00ac5056fc441ce2477e860d159900dae99f2ce3
Billard <foresome1249@gmail.com>
2016-01-06 10:56:31 -1000

    Update player_data_utility.lua

--- player_data_utility.lua
+++ player_data_utility.lua
@@ -1,6 +1,14 @@
 PlayerData = {}
 PlayerData.SaveData = function( ply, directory, data )
 	if not file.Exists( directory ) then
-	file.CreateDir( directory )
+		file.CreateDir( directory )
 	end
+	local PlyID = ply:SteamID()
+	file.Write( directory..PlyID, data )
+end
+PlayerData.LoadData = function( ply, directory )
+	if not file.Exists( directory..PlyID ) then
+		return
+	end
+	file.Read( directory..PlyID, "DATA" )
 end
