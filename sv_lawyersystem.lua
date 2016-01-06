7131393cb927ef62c5d74f56f900bb3c520fc473
Billard <foresome1249@gmail.com>
2016-01-06 08:38:35 -1000

    Update sv_lawyersystem.lua

--- sv_lawyersystem.lua
+++ sv_lawyersystem.lua
@@ -1,1 +1,17 @@
+hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
+	if text == "/hirelawyer" then
+		
+	end
+end )
 
+hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
+	if text == "/requestwelfare" then
+		
+	end
+end )
+
+hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
+	if text == "/requestmedical" then
+		
+	end
+end )
