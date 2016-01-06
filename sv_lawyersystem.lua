a2e1823f15e83df483481ce575c063478e64dc20
Billard <foresome1249@gmail.com>
2016-01-06 10:33:31 -1000

    Update sv_lawyersystem.lua

--- sv_lawyersystem.lua
+++ sv_lawyersystem.lua
@@ -1,4 +1,5 @@
 local LawyerSystem = {}
+local LawyerSystem.DataDirectory = "darkrp_lawyer_system/"
 local LawyerSystem.HirePriceMin = 600
 local LawyerSystem.HirePriceMax = 1800
 local LawyerSystem.FirstNames = {
@@ -24,6 +25,29 @@ local LawyerSystem.LastNames = {
 	["Potter"] = true
 }
 
+local function SavePlayerData( ply, directory, data )
+	if not file.Exists( directory ) then
+		file.CreateDir( directory )
+	end
+	local PlayerSteamID = ply:SteamID()
+	if not file.Exists( directory..PlayerSteamID ) then
+		return
+	end
+	file.Write( directory..PlayerSteamID..".txt", data )
+end
+
+local function LoadPlayerData( ply, directory )
+	if not file.Exists( directory ) then
+		return
+	end
+	local PlayerSteamID = ply:SteamID()
+	if not file.Exists( directory..PlayerSteamID ) then
+		return
+	end
+	local data = file.Read( directory..PlayerSteamID..".txt", "DATA" )
+	return data
+end
+
 local LawyerSystem.GetLawyerName = function()
 	local FirstName = math.random( 1, #LawyerSystem.FirstNames )
 	local MiddleName = math.random( 1, #LawyerSystem.MiddleNames )
@@ -32,12 +56,22 @@ local LawyerSystem.GetLawyerName = function()
 	return LawyerName
 end
 
+local LawyerSystem.HasLawyer = function( ply )
+	if LoadPlayerData( ply, LawyerSystem.DataDirectory ) == "true" then
+		return true
+	elseif LoadPlayerData( ply, LawyerSystem.DataDirectory ) == "false" then
+		return false
+	end
+end
+
 hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
 	if text == "/hirelawyer" then
 		local LawyerPrice = math.random( LawyerSystem.HirePriceMin, LawyerSystem.HirePriceMax )
 		if ply:canAfford( LawyerPrice ) then
 			ply:addMoney( LawyerPrice * -1 )
 			local LawyerName = LawyerSystem.GetName
+			ply:ChatPrint( LawyerName.." has been hired as your lawyer!" )
+			SavePlayerData( ply, LawyerSystem.DataDirectory, "true" )
 		else
 			ply:ChatPrint( "You cannot afford a lawyer!" )
 		end
@@ -46,7 +80,11 @@ end )
 
 hook.Add( "PlayerSay", "LawyerSystemWelfareCommand", function( ply, text, isTeam )
 	if text == "/requestwelfare" then
-		
+		if LawyerSystem.HasLawyer( ply ) then
+			
+		else
+			ply:ChatPrint( "You do not have a lawyer!" )	
+		end
 	end
 end )
 
