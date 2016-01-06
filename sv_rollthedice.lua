46d87c5f9dd927851122ee64ee37eccd5e65c0f6
Billard <foresome1249@gmail.com>
2016-01-05 18:50:55 -1000

    Update sv_rollthedice.lua

--- sv_rollthedice.lua
+++ sv_rollthedice.lua
@@ -15,14 +15,14 @@ local RTD.Rolls = { --Use this to add more rolls but only if you created the fun
 	[RTD_Drugged] = true
 ]
 
-function DisplayDiceMessage( ply, message_end )
+local function DisplayDiceMessage( ply, message_end )
 	for k, v in pairs( player.GetAll() ) do
 		local RandomNumber = math.random( 1, #RTD.Rolls )
 		v:ChatPrint( ply.." has rolled a "..RandomNumber.." and "..message_end )
 	end
 end
 
-function RTD_SpeedIncrease( ply )
+local function RTD_SpeedIncrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "became a cheetah!"
 	DisplayDiceMessage( ply, become_message )
@@ -30,7 +30,7 @@ function RTD_SpeedIncrease( ply )
 	targetPly:SetWalkSpeed( 400 )
 end
 
-function RTD_SpeedDecrease( ply )
+local function RTD_SpeedDecrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "became a snail!"
 	DisplayDiceMessage( ply, become_message )
@@ -38,35 +38,35 @@ function RTD_SpeedDecrease( ply )
 	targetPly:SetWalkSpeed( 4 )
 end
 
-function RTD_HealthIncrease( ply )
+local function RTD_HealthIncrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "ate a lucky sandvich!"
 	DisplayDiceMessage( ply, become_message )
 	targetPly:SetHealth( 400 )
 end
 
-function RTD_HealthDecrease( ply )
+local function RTD_HealthDecrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "became killable by a sneeze!"
 	DisplayDiceMessage( ply, become_message )
 	targetPly:SetHealth( 1 )
 end
 
-function RTD_JumpIncrease( ply )
+local function RTD_JumpIncrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "became a bunny!"
 	DisplayDiceMessage( ply, become_message )
 	targetPly:SetJumpPower( 800 )
-end
+local end
 
-function RTD_JumpDecrease( ply )
+local function RTD_JumpDecrease( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "became an anvil!"
 	DisplayDiceMessage( ply, become_message )
 	targetPly:SetJumpPower( 10 )
 end
 
-function RTD_Drugged( ply )
+local function RTD_Drugged( ply )
 	local targetPly = ply or thePlayer
 	local become_message = "took some of that good stuff!"
 	DisplayDiceMessage( ply, become_message )
@@ -75,16 +75,16 @@ function RTD_Drugged( ply )
 	net.Send( targetPly )
 end
 
-function EnableRTD()
+local function EnableRTD()
 	RTD.Enabled = true
 end
 
-function DisableRTD()
+local function DisableRTD()
 	RTD.Enabled = false	
 	timer.Simple( RTD.CooldownTime, EnableRTD )
 end
 
-function PickRandomRTD( ply )
+local function PickRandomRTD( ply )
 	local RandomNumber = math.random( 1, #RTD.Rolls )
 	local FuncToCall = RTD.Rolls[RandomNumber]
 	thePlayer = ply
