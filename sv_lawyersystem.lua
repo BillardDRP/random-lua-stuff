405c1fd1fdb89b22fbc917d8982c2b065ebbf7d4
Billard <foresome1249@gmail.com>
2016-01-06 08:40:54 -1000

    Update sv_lawyersystem.lua

--- sv_lawyersystem.lua
+++ sv_lawyersystem.lua
@@ -1,16 +1,20 @@
+local LawyerSystem = {}
+local LawyerSystem.HirePriceMin = 600
+local LawyerSystem.HirePriceMax = 1800
+
 hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
 	if text == "/hirelawyer" then
 		
 	end
 end )
 
-hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
+hook.Add( "PlayerSay", "LawyerSystemWelfareCommand", function( ply, text, isTeam )
 	if text == "/requestwelfare" then
 		
 	end
 end )
 
-hook.Add( "PlayerSay", "LawyerSystemHireCommand", function( ply, text, isTeam )
+hook.Add( "PlayerSay", "LawyerSystemMedicalCommand", function( ply, text, isTeam )
 	if text == "/requestmedical" then
 		
 	end
