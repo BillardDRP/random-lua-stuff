f197c53e46566a68ee488617d9f3e636d6306999
Billard <foresome1249@gmail.com>
2016-01-08 10:35:02 -1000

    Update billardhack.lua

--- billardhack.lua
+++ billardhack.lua
@@ -1,1 +1,6 @@
+--draw.RoundedBox( number cornerRadius, number x, number y, number width, number height, table color )
 
+hook.Add( "HUDPaint", "BillardHack_Crosshair" function()
+	draw.RoundedBox( 2, ScrW(), ScrH(), 30, 2, Color( 255, 0, 0, 180 ) )
+	draw.RoundedBox( 2, ScrW(), ScrH(), 2, 30, Color( 255, 0, 0, 180 ) )
+end )
