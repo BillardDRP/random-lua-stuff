b264bbe0538de08652f34d4ff376c51ae49e1c83
Billard <foresome1249@gmail.com>
2016-01-07 17:20:26 -1000

    Update xp_system.lua

--- xp_system.lua
+++ xp_system.lua
@@ -1,1 +1,25 @@
+local XP = {}
+local XP.Reward = {}
+local XP.Penalty = {}
+local XP.Directory = "billard_xp_system/"
+local XP.PrestigeDirectory
+local XP.Reward.KillPlayer = 60
+local XP.Reward.KillNPC = 10
+local XP.Penalty.Death = 0 --No penalty for dying
 
+
+local XP.SavePlayerData = function( ply, directory, data )
+	if not file.Exists( directory ) then
+		file.CreateDir( directory )
+	end
+	local PlyID = ply:SteamID()
+	file.Write( directory..PlyID, data )
+end
+
+XP.LoadPlayerData = function( ply, directory )
+	if not file.Exists( directory..PlyID ) then
+		return
+	end
+	local data = file.Read( directory..PlyID, "DATA" )
+	return data
+end
