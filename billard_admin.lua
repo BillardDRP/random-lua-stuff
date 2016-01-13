6c8021103c58ea343dd3f2cc0d1f1f746af01ac6
Billard <foresome1249@gmail.com>
2016-01-13 09:37:41 -1000

    Update billard_admin.lua

--- billard_admin.lua
+++ billard_admin.lua
@@ -1,1 +1,21 @@
-
+local function findplayer( name )
+	name = string.lower( name )
+	for k,v in pairs( player.GetAll() ) do
+		if string.lower(v:SteamID() ) == name then
+			return v
+		end
+		if string.lower( v:UniqueID() ) == name then
+			return v
+		end
+		local nick = string.lower( v:Nick()  )
+		if string.find( nick, name ) then
+			return v
+		end
+		if v.SteamName then
+			local sname = string.lower( v:SteamName() )
+			if string.find( sname, name ) then
+				return v
+			end
+		end
+	end
+end
