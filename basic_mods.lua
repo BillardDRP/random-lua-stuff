134580c37ba98048069e36d9565c34b47b70f5e5
Billard <foresome1249@gmail.com>
2016-01-14 11:38:19 -1000

    Update basic_mods.lua

--- basic_mods.lua
+++ basic_mods.lua
@@ -2,13 +2,18 @@
 
 basic_mods = {} -- Global
 
-basic_mods["create"] = function( ply, cmd, args ) -- Built to be console command
+local function CheckPly( ply )
 	if not IsValid( ply ) then
-		return
+		return false
 	end
 	if not ply:IsAdmin() then
-		return
+		return false
 	end
+	return true
+end
+
+basic_mods["create"] = function( ply, cmd, args ) -- Built to be console command
+	if not CheckPly( ply ) then return end
 	local thing = ""
 	local model = ""
 	if not args[1] then
@@ -28,12 +33,7 @@ basic_mods["create"] = function( ply, cmd, args ) -- Built to be console command
 end
 
 basic_mods["burn"] = function( ply, cmd, args )
-	if not IsValid( ply ) then
-		return
-	end
-	if not ply:IsAdmin() then
-		return
-	end
+	if not CheckPly( ply ) then return end
 	local length = 1
 	if not args[1] then
 		length = 5
@@ -54,12 +54,7 @@ basic_mods["burn"] = function( ply, cmd, args )
 end
 
 basic_mods["freeze"] = function( ply, cmd, args )
-	if not IsValid( ply ) then
-		return
-	end
-	if not ply:IsAdmin() then
-		return
-	end
+	if not CheckPly( ply ) then return end
 	local length = 1
 	if not args[1] then
 		length = 5
@@ -83,13 +78,8 @@ basic_mods["freeze"] = function( ply, cmd, args )
 end
 
 basic_mods["model"] = function( ply, cmd, args )
-	if not IsValid( ply ) then
-		return
-	end
-	if not ply:IsAdmin() then
-		return
-	end
-	local model = "models/error.mdl"
+	if not CheckPly( ply ) then return end
+	local model = ""
 	if not args[1] then
 		model = "models/error.mdl"
 	else
@@ -103,6 +93,7 @@ basic_mods["model"] = function( ply, cmd, args )
 end
 
 -- Console commands
+local concommand = concommand -- I think this makes it go faster
 concommand.Add( "basic_create", basic_mods.create )
 concommand.Add( "basic_burn", basic_mods.burn )
 concommand.Add( "basic_freeze", basic_mods.freeze )
