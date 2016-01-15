9e3297b547e358c0bf376bc0e100fa1987fed904
Billard <foresome1249@gmail.com>
2016-01-14 15:32:21 -1000

    Update simple_base.lua

--- simple_base.lua
+++ simple_base.lua
@@ -1,1 +1,50 @@
+-- Spawning info
 
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
+-- SWEP info
+
+SWEP.ClassName = "simple_base"
+SWEP.PrintName = "Simple SWEP Base"
+SWEP.Base = "weapon_base"
+
+-- Personal info
+
+SWEP.Author = "<Your name here>"
+SWEP.Contact = "www.google.com"
+SWEP.Purpose = "Shoot"
+SWEP.Instructions = "Point and click"
+
+-- Model info
+
+SWEP.ViewModel = "models/weapons/v_pistol.mdl"
+SWEP.ViewModelFlip = false
+SWEP.ViewModelFOV = 62
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
+
+-- Switching info
+
+SWEP.AutoSwitchFrom = true
+SWEP.AutoSwitchTo = true
+SWEP.Weight = 5
+
+-- Viewmodel animations
+
+SWEP.BobScale = 1
+SWEP.SwayScale = 1
+
+-- Info box info
+
+SWEP.BounceWeaponIcon = true
+SWEP.DrawWeaponInfoBox = true
+
+-- Drawing info
+
+SWEP.DrawAmmo = true
+SWEP.DrawCrosshair = true
+
+-- Slot info
+
+SWEP.Slot = 0
+SWEP.SlotPos = 10
