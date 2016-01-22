6c6c92cee37cfef92edda0ceac68accc25d3d5c7
Billard <foresome1249@gmail.com>
2016-01-21 15:36:08 -1000

    Update olivescript.lua

--- olivescript.lua
+++ olivescript.lua
@@ -2,6 +2,10 @@ local olivescript = {
 	version = "0.1",
 	description = "OliveScript is a stupid little language that does stupid little things.",
 	author = "Sir Francis Billard",
+	garrysmod = {
+		enabled = true,
+		admin = true,
+	},
 }
 
 local olivescript.keywords = {
@@ -9,7 +13,7 @@ local olivescript.keywords = {
 	["->"] = " then ",
 	["&8"] = " while ",
 	["-->"] = " do ",
-	["-|"] = " ( ",
+	["-|"] = "( ",
 	["|-"] = " ) ",
 	["++"] = " and ",
 	["^^"] = " or ",
@@ -22,9 +26,11 @@ local olivescript.compile = function( code )
 	if not code then return end
 	local newcode = tostring( code )
 	for k, v in pairs( olivescript.keywords ) do
-		string.gsub( tostring( code ), tostring( k ), tostring( v ) )
+		newcode = string.gsub( tostring( code ), tostring( k ), tostring( v ) )
 	end
+	print( tostring( newcode ) )
 	print( "Compiled Olivescript into Lua in " .. tonumber( os.clock() ) - timeStarted  .. " seconds." )
+	return newcode
 end
 
 local olivescript.decompile = function( code )
@@ -32,7 +38,18 @@ local olivescript.decompile = function( code )
 	if not code then return end
 	local newcode = tostring( code )
 	for k, v in pairs( olivescript.keywords ) do
-		string.gsub( tostring( code ), tostring( v ), tostring( k ) )
+		newcode = string.gsub( tostring( code ), tostring( v ), tostring( k ) )
 	end
+	print( tostring( newcode ) )
 	print( "Compiled Lua into Olivescript in " .. tonumber( os.clock() ) - timeStarted  .. " seconds." )
+	return newcode
+end
+
+if olivescript.garrysmod.enabled then
+	concommand.Add( "olivescript_compile", function( ply, cmd, args, argsStr )
+		if not IsValid( ply ) then return end
+		if olivescript.garrysmod.admin and not ply:IsAdmin() then return end
+		local code = olivescript.compile( tostring( argsStr ) )
+		RunString( code )
+	end )
 end
