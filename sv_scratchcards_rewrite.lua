6f4e9519b2b9f2d45f1b8c7cb5e982637b07d860
Billard <foresome1249@gmail.com>
2016-05-24 10:39:24 -1000

    Update sv_scratchcards_rewrite.lua

--- sv_scratchcards_rewrite.lua
+++ sv_scratchcards_rewrite.lua
@@ -1,1 +1,17 @@
 --Rewriting the scratchcards module to make it slightly less shitty.
+
+ScratchCards = {}
+ScratchCards.Error = {}
+ScratchCards.Error.InvalidPlayer = "[ERROR] Function ScratchCards.Set got invalid player!"
+ScratchCards.Debug = true
+ScratchCards.Price = 1000
+ScratchCards.PrizeMin = 5000
+ScratchCards.PrizeMax = 500000
+ScratchCards.Chance = 20
+ScratchCards.ChanceOutOf = 100
+ScratchCards.DefaultGetData = 0 --What to get from ScratchCards.Get if the PData doesn't exist
+ScratchCards.Set = function(ply, amt)
+	if ScratchCards.Debug and not IsValid(ply) then print(ScratchCards.Error.InvalidPlayer) return end
+	if ScratchCards.Debug and not IsPlayer(ply) then print("[ERROR] Function ScratchCards.Set got a non-player entity!") return end
+	ply:SetPData("scratchcards_amount", amt)
+end
